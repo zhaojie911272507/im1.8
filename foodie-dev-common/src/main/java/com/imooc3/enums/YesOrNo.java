e1dadb21cd342ee1f9cb0b2dc9ba8c0b447f7f05
zhaojie911272507 <911272507@qq.com>
2020-07-22 21:38:25 +0800

    商品推荐-实现接口与联调

---
+++ foodie-dev-common/src/main/java/com/imooc3/enums/YesOrNo.java
@@ -0,0 +1,20 @@
+package com.imooc3.enums;
+
+/**
+ * @Desc：是否枚举
+ *
+ */
+public enum YesOrNo {
+
+    NO(0,"否"),
+    YES(1,"是");
+
+
+    public final Integer type;
+    public final String value;
+
+    YesOrNo(Integer type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
---
+++ foodie-dev-service/src/main/java/com/imooc3/service/CategoryService.java
@@ -0,0 +1,31 @@
+package com.imooc3.service;
+
+import com.imooc3.pojo.Category;
+import com.imooc3.pojo.vo.CategoryVO;
+import com.imooc3.pojo.vo.NewItemsVO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    /**
+     * 查询所有一级分类
+     * @return
+     */
+    public List<Category> queryAllRootLevelCat();
+
+    /**
+     * 根据一级分类id查询子分类信息
+     * @param rootCatId
+     * @return
+     */
+    public List<CategoryVO> getSubCatList(Integer rootCatId);
+
+    /**
+     * 查询首页每个一级分类下的6条最新商品数据
+     * @param rootCatId
+     * @return
+     */
+    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId);
+
+}
